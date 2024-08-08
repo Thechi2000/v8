0fc5fe8e457a4696aa04a7c9fd2894044a66c662
Ludovic Mermod <ludovic.mermod@gmail.com>
2024-08-08 15:15:24 +0200

    fix: flag name

--- test/mjsunit/regexp-lookbehind-experimental.js
+++ test/mjsunit/regexp-lookbehind-experimental.js
@@ -25,7 +25,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Flags: --allow-natives-syntax --default-to-experimental-regexp-engine --experimental_regexp_engine_capture_group_opt
+// Flags: --allow-natives-syntax --default-to-experimental-regexp-engine --experimental-regexp-engine-capture-group-opt
 
 // Tests captures in positive and negative look-ahead in regular expressions.
 
